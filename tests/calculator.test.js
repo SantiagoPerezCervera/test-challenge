4105436927cbec5f22435735e6934d988c497e98
SantiagoPerez <sany.pc26@gmail.com>
2023-03-02 10:19:36 -0600

    v1

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,24 +1,24 @@
-const { default: expect } = require('expect');
+const { substract, multiply, divide, power } = require('../calculator');
 
 const calculator = require('../calculator');
 
 test('adds a + b', () => {
-    //
+    expect(calculator.sum(4,6)).toBe(10);
 })
 
 test('substracts a - b', () => {
-   //
+    expect(substract(4,6)).toBe(-2);
 })
 
 test('multiplies a * b', () => {
-    //
+    expect(multiply(4,6)).toBe(24);
 })
 
 test('divides a / b', () => {
-    // Consider decimals
+    expect(divide(2,1)).toBe(2);
 })
 
 test('Raises to power a ^ b', () => {
-    //
+    expect(power(4,6)).toBe(4096);
 })
 
